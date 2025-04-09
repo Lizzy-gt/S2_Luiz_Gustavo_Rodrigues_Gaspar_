94a487dd4a6af2babc7823d5c2d6c67b2e24df9f
Lizzy-gt <luiz.gaspar@aluno.senai.br>
2025-04-09 12:47:34 -0300

    Projeto Avaliativo!

---
+++ Exercicio/Projeto.py
@@ -0,0 +1,8 @@
+# -*- coding: UTF-8 -*-
+print ("Vamos ver a temperatura para Fahrenheit ou Celsius!")
+cel = float(input("Fale a sua temperatura: "))
+temp = input("Quer em Fahrenheit (F) ou Celsius (C)!")
+def tempe ():
+    global cel, fc
+    if temp == "C" or temp == 'c':
+        mult = 1.8 * cel + 32
